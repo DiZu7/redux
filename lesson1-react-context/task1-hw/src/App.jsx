7e72da051a1f5e5170484cde7ac20cd5f83b8df3
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-19 17:21:25 +0200

    test

---
+++ lesson1-react-context/task1-hw/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { UserContext } from './context';
+import Header from './Header';
+import './index.scss';
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nicola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.userData}>
+        <Header />
+      </UserContext.Provider>
+    );
+  }
+}
+
+export default App;
