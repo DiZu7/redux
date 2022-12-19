45005c95e186b38853d9fe1a39f9b61d5f8a8e87
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-19 17:24:20 +0200

    test

--- lesson1-react-context/task1-hw/src/App.jsx
+++ lesson1-react-context/task1-hw/src/App.jsx
@@ -5,15 +5,15 @@ import './index.scss';
 
 class App extends React.Component {
   state = {
-    userData: {
-      name: 'Nicola Tesla',
-      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-    },
+    // userData: {
+    name: 'Nicola Tesla',
+    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    // },
   };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.userData}>
+      <UserContext.Provider value={this.state}>
         <Header />
       </UserContext.Provider>
     );
