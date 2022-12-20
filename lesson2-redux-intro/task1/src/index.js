b3fc7471ac388189e2d6c04881aa6f1f6eb447f6
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-20 15:30:59 +0200

    test

---
+++ lesson2-redux-intro/task1/src/index.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'COUNTER/INCREMENT':
+      return state + 1;
+    case 'COUNTER/DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({ reducer: counterReducer });
+
+export default store;
