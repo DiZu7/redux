1dbed8557d278839f96021b34f2ecfb98ae03f63
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-23 13:12:46 +0200

    test

---
+++ lesson4-react-with-redux/task2/src/users/Users.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../index.scss';
+import { connect } from 'react-redux';
+import * as usersActions from './users.action';
+
+class Users extends React.Component {
+  onUserCreate = () => {
+    const id = Math.round(Math.random() * 1000000);
+    const newUser = {
+      id,
+      name: `User # ${id}`,
+    };
+
+    this.props.createUser(newUser);
+  };
+
+  render() {
+    const { users, deleteUser } = this.props;
+    return (
+      <div className="users">
+        <button className="users__create-btn" onClick={this.onUserCreate}>
+          Create user
+        </button>
+        <ul className="users__list">
+          {users.map(user => (
+            <li key={user.id} className="users__list-item">
+              <span>{user.name}</span>
+              <button className="users__delete-btn" onClick={() => deleteUser(user.id)}>
+                +
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    users: state.users.usersList,
+  };
+};
+
+const mapDispatch = {
+  createUser: usersActions.addUser,
+  deleteUser: usersActions.deleteUser,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+const ConnectedUsers = connector(Users);
+
+export default ConnectedUsers;
---
+++ lesson4-react-with-redux/task2/src/users/users.reducer.js
@@ -0,0 +1,27 @@
+import { ADD_USER, DELETE_USER } from './users.action';
+
+const initialState = {
+  usersList: [],
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER: {
+      return {
+        ...state,
+        usersList: state.usersList.concat(action.payload.userData),
+      };
+    }
+
+    case DELETE_USER: {
+      return {
+        ...state,
+        usersList: state.usersList.filter(user => user.id !== action.payload.userId),
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
