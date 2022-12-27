a2978de96263c379aace081e822d45f6ce0a5042
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-27 17:57:23 +0200

    test

--- lesson6-middleware/task1-hw/src/weather/Weather.jsx
+++ lesson6-middleware/task1-hw/src/weather/Weather.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import '../index.scss';
 import { getWeatherData } from './weather.actions';
 import { getWeatherDataSelector } from './weather.selectors';
 
-class Weather extends React.Component {
-  render() {
-    return (
-      <main className="weather">
-        <h1 className="weather__title">Weather data</h1>
-        <ul className="cities-list">
-          {this.props.weatherData.map(cityWeather => (
-            <li key={cityWeather.key} className="city">
-              <span className="city__name">{cityWeather.name}</span>
-              <span className="city__temperature">{cityWeather.temperature}</span>
-            </li>
-          ))}
-        </ul>
-      </main>
-    );
-  }
-}
+const Weather = ({ weatherData, getWeatherData }) => {
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
+  return (
+    <main className="weather">
+      <h1 className="weather__title">Weather data</h1>
+      <ul className="cities-list">
+        {weatherData.map(({ id, name, temperature }) => (
+          <li key={id} className="city">
+            <span className="city__name">{name}</span>
+            <span className="city__temperature">{`${temperature} F`}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
 const mapState = state => {
   return {
     weatherData: getWeatherDataSelector(state),
