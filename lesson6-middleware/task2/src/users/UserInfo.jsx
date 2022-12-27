46ee069ab29c869eed162ee74a77d71508b9db94
DiZu7 <dianadovhanyuk@gmail.com>
2022-12-27 14:55:36 +0200

    test

--- lesson6-middleware/task2/src/users/UserInfo.jsx
+++ lesson6-middleware/task2/src/users/UserInfo.jsx
@@ -13,13 +13,12 @@ const UserInfo = ({ isFetching, userData }) => {
     return null;
   }
 
-  const { name, location, avatar_url } = userData;
   return (
     <div className="user">
-      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <img alt="User Avatar" src={userData.avatar_url} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">{name}</span>
-        <span className="user__location">{`from ${location}`}</span>
+        <span className="user__name">{userData.name}</span>
+        <span className="user__location">{userData.location}</span>
       </div>
     </div>
   );
